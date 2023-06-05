45b65b0de6e2f425e379ebc645984def8bcb9efe
Brian Miranda <brianmiranda.dev@gmail.com>
2023-06-05 17:58:19 -0400

    refactor: Componentize the circle svg
    The svg code was taking too much space, so the
    ProgressCircle.jsx component was created.

---
+++ src/components/ProgressCircle.jsx
@@ -0,0 +1,11 @@
+const ProgressCircle = () => {
+  return (
+    <svg>
+      <circle r="49.45%" cx="50%" cy="50%" className="track"></circle>
+      <circle r="49.45%" cx="50%" cy="50%" className="progress-circle"></circle>
+      <circle r="49.45%" cx="50%" cy="50%" className="break-progress"></circle>
+    </svg>
+  );
+};
+
+export default ProgressCircle;
